2b5fe57226b1d7d5897314758ba4a97d6eca73ae
B Hari prasad <39781@hexaware.com>
2018-02-14 12:25:06 +0530

    first commit

--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ router.get('/',function(req, res){
 
 router.post('/botHandler',function(req, res){
 	//console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
-	//console.log('Dialogflow Request body: ' + JSON.stringify(req.body));		
+	console.log('Dialogflow Request body: ' + JSON.stringify(req.body));		
 	res.status(200);	
 			
 	if (req.body.result||req.body.queryResult) {
