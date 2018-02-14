fb715b388b0f9c8918a8d700cdcd9b130bb3c096
B Hari prasad <39781@hexaware.com>
2018-02-14 09:09:16 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var config ={
+	
+	serviceNow : {		
+		category 		: ['inquiry/Help','Software','Hardware','Network','Database'],
+		subCategory		: ['Antivirus','Email','Internal Application'],
+		contactType 	: ['Email','Phone','Self-service','Walk-in'],		
+		impact 			: ['High','Medium','Low'],
+		urgency 		: ['High','Medium','Low'],		
+		caller			: ['Hariprasad','Harikrishna']		
+	},	
+}
+module.exports = config;
