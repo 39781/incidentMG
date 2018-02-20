506c7fc0a65e47435754046030666919a4961e7d
B Hari prasad <39781@hexaware.com>
2018-02-20 10:37:44 +0530

    first commit

--- google.js
+++ google.js
@@ -77,7 +77,57 @@ responses.inputPrompts = function(sessionId,  req, res){
 responses.getFinalCardResponse = function(textMsg, callBackIntent, params){
 	return new Promise(function(resolve, reject){
 		var data = textMsg.split(';');
-		var rsp = {
+		var rsp{
+			"speech":"",
+			"data":{
+  "google": {
+  "expect_user_response": true,
+  "rich_response": {
+  "items": [
+    {
+      "simpleResponse": {
+          "textToSpeech":"This is the first simple response for a basic card"
+      }
+    },
+    {
+      "basicCard": {
+        "title":"Title: this is a title",
+        "formattedText":"This is a basic card.  Text in a\n      basic card can include \"quotes\" and most other unicode characters\n      including emoji 📱.  Basic cards also support some markdown\n      formatting like *emphasis* or _italics_, **strong** or __bold__,\n      and ***bold itallic*** or ___strong emphasis___ as well as other things\n      like line  \nbreaks",
+        "subtitle":
+        "This is a subtitle",
+        "image": {
+          "url":"https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+          "accessibilityText":"Image alternate text"
+        },
+        "buttons": [
+          {
+            "title":"This is a button",
+            "openUrlAction":{
+              "url":"https://assistant.google.com/"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "simpleResponse": {
+        "textToSpeech":"This is the 2nd simple response ",
+        "displayText":"This is the 2nd simple response"
+      }
+    }
+  ],
+  "suggestions":
+  [
+    {"title":"Basic Card"},
+    {"title":"List"},
+    {"title":"Carousel"},
+    {"title":"Suggestions"}
+  ]
+}
+}
+}
+		}
+		/*var rsp = {
 			"speech":"",
 			"data":{
 			  "google": {
@@ -111,7 +161,7 @@ responses.getFinalCardResponse = function(textMsg, callBackIntent, params){
 					}
 				}
 			}
-		};
+		};*/
 
 		/*var rsp ={
 			"speech": "",
