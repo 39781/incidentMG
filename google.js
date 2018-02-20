aa0ada68770b31402be6e0138b207c48b8141b52
B Hari prasad <39781@hexaware.com>
2018-02-20 10:14:34 +0530

    first commit

--- google.js
+++ google.js
@@ -78,34 +78,53 @@ responses.getFinalCardResponse = function(textMsg, callBackIntent, params){
 	return new Promise(function(resolve, reject){
 		var data = textMsg.split(';');
 		var rsp = {
-      "speech": "",
-      "messages": [
-        {
-          "type": "basic_card",
-          "platform": "google",
-          "title": "Incident Number :  INC0012419",
-          "subtitle": "Incident Created",
-          "formattedText": "please Note for future reference Thank you for using me, I can help you please choose any one option",
-          "buttons": []
+  "google": {
+  "expect_user_response": true,
+  "rich_response": {
+  "items": [
+    {
+      "simpleResponse": {
+          "textToSpeech":"This is the first simple response for a basic card"
+      }
+    },
+    {
+      "basicCard": {
+        "title":"Title: this is a title",
+        "formattedText":"testing",
+        "subtitle":
+        "This is a subtitle",
+        "image": {
+          "url":"https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+          "accessibilityText":"Image alternate text"
         },
-        {
-          "type": "suggestion_chips",
-          "platform": "google",
-          "suggestions": [
-            {
-              "title": "Create Incident"
-            },
-            {
-              "title": "Track Incident"
+        "buttons": [
+          {
+            "title":"This is a button",
+            "openUrlAction":{
+              "url":"https://assistant.google.com/"
             }
-          ]
-        },
-        {
-          "type": 0,
-          "speech": ""
-        }
-      ]
+          }
+        ]
+      }
+    },
+    {
+      "simpleResponse": {
+        "textToSpeech":"This is the 2nd simple response ",
+        "displayText":"This is the 2nd simple response"
+      }
     }
+  ],
+  "suggestions":
+  [
+    {"title":"Basic Card"},
+    {"title":"List"},
+    {"title":"Carousel"},
+    {"title":"Suggestions"}
+  ]
+}
+}
+}
+
 		/*var rsp ={
 			"speech": "",
 			"messages": [{
