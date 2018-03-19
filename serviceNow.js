a849ad8c2e9636215c106450405a5e9ba55a17cf
Your Name <you@example.com>
2018-03-19 11:31:32 +0530

    first commit

--- serviceNow.js
+++ serviceNow.js
@@ -39,9 +39,9 @@ var serviceNowApi = {
 						resolve(txtMsg);
 				}else{									
 					if(body.indexOf("Sorry, I got bored and fell asleep")>=0){
-						txtMsg = "Error  ; Sorry, I got bored and fell asleep. Sign in to the Developer Site and I will be ready to report for duty! Examine the FAQ to learn more about sleepy instances; incident Created";
+						txtMsg = "Error  ; Sorry, I got bored and fell asleep. Sign in to the Developer Site and I will be ready to report for duty! Examine the FAQ to learn more about sleepy instances; incident Creation";
 					}else if(body.error){
-						txtMsg = "Error  ; Try again ; Incident Created";						
+						txtMsg = "Error  ; Try again ; Incident Creation";						
 					}else{						
 						txtMsg = "Incident Number : "+body.result.number+"; Please note for future reference; Incident Created";
 					}					
